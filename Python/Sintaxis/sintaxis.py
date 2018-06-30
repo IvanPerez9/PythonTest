fa4989ebe73811814fc71fc198518c477477dfd2
IvanPerez9 <ivan120695@gmail.com>
2018-06-30 10:43:19 +0200

    Sintaxis

---
+++ Python/Sintaxis/sintaxis.py
@@ -0,0 +1,15 @@
+print ("Hola mundo");
+print ("hola mundo"); print("Adios")
+# Esto es un comentario 
+#Separar varias lineas con \ 
+
+mi_nombre = "Hola soy Ivan";
+
+#mi nombre es una variable
+
+a = 0;
+for i in range(5):
+    a+=1;
+    print(a);
+    
+    
