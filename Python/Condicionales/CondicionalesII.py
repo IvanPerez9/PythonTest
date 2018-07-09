776c92a6cfcf77150d3aa8060cdb88d57fc17e5d
IvanPerez9 <ivan120695@gmail.com>
2018-07-09 12:39:46 +0200

    Condicionales II

---
+++ Python/Condicionales/CondicionalesII.py
@@ -0,0 +1,21 @@
+'''
+Created on 9 jul. 2018
+
+@author: ivan1
+'''
+
+#Intrucciones else y elif 
+
+print("Verificacion")
+
+edad= int(input("Introduce tu edad "))
+
+if edad < 18 :
+    print("No puedes pasar")
+elif edad > 100:
+    print("No puedes pasar")
+else:
+    print("Puedes pasar")
+print("--Programa finalizado")
+
+  
