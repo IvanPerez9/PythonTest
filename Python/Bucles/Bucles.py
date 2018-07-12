074df27c583a4bb0165918c8a0be39d15c35668f
Iván Pérez Huete <ivan120695@gmail.com>
2018-07-12 13:52:00 +0200

    Start with loops

---
+++ Python/Bucles/Bucles.py
@@ -0,0 +1,18 @@
+'''
+Created on 12 jul. 2018
+
+@author: ivan1
+'''
+
+#Tipos determinados (numero determinado de veces) e indeterminados (No se sabe a priori) 
+
+# Se parece a un for-each 
+
+for i in [1,2,3]:
+    print("Hola")
+
+for i in ["primavera", "verano", "Otono", "invierno"]:
+    print("Hola")
+
+for i in ["primavera", "verano", "Otono", "invierno"]:
+    print(i)
