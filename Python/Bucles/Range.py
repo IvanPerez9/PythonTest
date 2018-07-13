6885a834112980586798242c2376dab59f302fee
Iván Pérez Huete <ivan120695@gmail.com>
2018-07-13 18:41:33 +0200

    Range

---
+++ Python/Bucles/Range.py
@@ -0,0 +1,19 @@
+'''
+Created on 13 jul. 2018
+
+@author: ivan1
+'''
+from Bucles.Bucles import email
+
+for i in range(5):
+    print("hola") # Parecido a un array 
+    print(f"valor de la variable {i}") #F es tipo especial, y las {} concatenan con el texto
+
+#VARIABLES DE TIPO TEXTO CON FUNCIONES TIPO F 
+
+for i in range (5,50,3): #De 3 en tres desde 5 a 49  
+    print("hola")
+
+#Funcion len , longitud de un String , esto ojo 
+for i in range(len(email)):
+    print()
