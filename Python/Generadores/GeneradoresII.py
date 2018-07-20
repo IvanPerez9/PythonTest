c251a35b911583d955fcc56dc2882f75d718f3d0
Iván Pérez Huete <ivan120695@gmail.com>
2018-07-20 11:07:28 +0200

    yield from

--- Python/Generadores/GeneradoresII.py
+++ Python/Generadores/GeneradoresII.py
@@ -3,3 +3,15 @@ Created on 20 jul. 2018
 
 @author: ivan1
 '''
+
+#Intruccion Yield From 
+#Simplifica el codigo de los generadores en caso de utilizar bucles anidados 
+
+def DevolverCiudades (*ciudades): #Con * numero indeterminado de elementos y con tupla
+    for elemento in ciudades:
+        yield from elemento #Coge el subelemento 
+
+ciudadesDevueltas = DevolverCiudades("Madrid" , "BCN" , "Bilbao" , "Valencia")
+
+print(next(ciudadesDevueltas))
+
