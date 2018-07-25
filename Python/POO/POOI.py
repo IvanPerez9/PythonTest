4a36d783a2ae5949ea188f8e8994dd50d4a51cd0
Iván Pérez Huete <ivan120695@gmail.com>
2018-07-25 10:18:00 +0200

    POO

--- Python/POO/POOI.py
+++ Python/POO/POOI.py
@@ -9,25 +9,39 @@ Created on 24 jul. 2018
 
 class Coche ():
     
-    largoChasis = 250;
-    anchoChasis = 120;
-    ruedas = 4;
-    enmarcha= False;
+    #Constructor
+    #Encapsular una propiedad, proteger para no ser modificada desde fuera. Por ejemplo ruedas Video 27 18 min
+    #Ojo a esto, accesible desde dentro no desde fuera
+    
+    def __init__ (self):
+        self.__largoChasis = 250;
+        self.__anchoChasis = 120;
+        self.__ruedas = 4;  #Desde fuera de la clase no se puede modificar 
+        self.enmarcha= False;
     
     #Comportamiento == Metodos
     # This es Self , obligado a ponerlo
     
-    def arrancar(self):
-        self.enmarcha = True;
-        
-    def estado (self):
+    def arrancar(self,arrancamos):
+        self.enmarcha = arrancamos;
         if(self.enmarcha):
             return "El coche esta en marcha"
         else:
             return "El coche esta parado"
+        
+    def estado (self):
+        print("El coche tiene " , self.__ruedas , "ruedas")
+        print("El coche tiene " , self.__anchoChasis , "cm de ancho")
+        print("El coche tiene " , self.__largoChasis , "cm de largo")
 
-    
 miCoche=Coche()
 print(miCoche.largoChasis);
-miCoche.arrancar()
-print(miCoche.estado());
+print(miCoche.arrancar(True))
+miCoche.estado()
+
+print("Acontinuacion creamos el segundo objeto")
+
+miCoche2 = Coche();
+print(miCoche2.arrancar(False))
+miCoche2.ruedas = 2; #Variables de __ y sin son variables distintas
+miCoche2.estado()
