21c910c57f67b9519c4630aa902f41978f90281d
Iván Pérez Huete <ivan120695@gmail.com>
2018-07-27 10:23:17 +0200

    Cadenas

---
+++ Python/POO/Cadenas.py
@@ -0,0 +1,13 @@
+'''
+Created on 27 jul. 2018
+
+@author: ivan1
+'''
+
+#Se toma las cadenas como objetos 
+
+#Video 33 
+
+nombreObjeto = input("Introduce tu nombre")
+
+print("El bombre es: " , nombreObjeto.capitalize())
